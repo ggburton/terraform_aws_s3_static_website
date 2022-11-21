3c96c0cf4f5c2ccb37dc3f3f88fc2a5e5a9d2878
Greg Burton <gburton@combitel.com.au>
2022-11-22 07:41:26 +1100

    change ssl support method to sni-only

--- cloudfront.tf
+++ cloudfront.tf
@@ -36,7 +36,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate.cert.arn
-    ssl_support_method  = "vip"
+    ssl_support_method  = "sni-only"
   }
 
   restrictions {
